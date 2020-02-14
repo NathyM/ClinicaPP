450b6b336ecf043c314a6104c248dcc22330d70f
Natalie Macedo <57413004+NathyM@users.noreply.github.com>
2020-02-14 10:49:41 -0300

    mensagem do commit

---
+++ src/Control/FolhaPagamentoControl.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Control;
+
+import Model.FolhaPagamentoModel;
+
+/**
+ *
+ * @author 1191164435
+ */
+public class FolhaPagamentoControl {
+    private FolhaPagamentoModel fpm;
+    private double salarioBruto;
+    
+    public void setSalarioBruto(double salarioBruto){
+        this.salarioBruto = salarioBruto;   
+    }
+    public FolhaPagamentoModel calcularSalario(){
+        fpm = new FolhaPagamentoModel(salarioBruto);
+        return fpm;
+    }
+}
